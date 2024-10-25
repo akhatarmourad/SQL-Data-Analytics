86a914dee6c1fc1dced7ab5e7e838ace8308ec3a
akhatarmourad <mourad.akhatar01@gmail.com>
2024-10-25 15:47:36 +0100

    examples with sales tables

--- CTEs/2_CTEs.sql
+++ CTEs/2_CTEs.sql
@@ -9,4 +9,19 @@ SELECT * FROM emp e, average_salary av WHERE e.salary > av.avg_sal;
 -- Select all sales
 SELECT * FROM sales;
 
--- Fetch stores who's sales greater than average sales across all stores
+-- Without WITH Clause : Fetch stores who's sales greater than average sales across all stores
+-- 1- Get total sales for each store
+SELECT SUM(s.cost) AS total_sales_per_store FROM sales s GROUP BY s.store_id;
+
+-- 2- Get average sales across all stores
+SELECT CAST(AVG(total_sales_per_store) AS INT) AS average_sales
+	FROM (SELECT SUM(s.cost) AS total_sales_per_store FROM sales s GROUP BY s.store_id);
+
+-- 3- Fetch stores that surpass the average sales
+SELECT * FROM
+	(SELECT SUM(s.cost) AS total_sales_per_store FROM sales s GROUP BY s.store_id)
+	JOIN (
+		SELECT CAST(AVG(total_sales_per_store) AS INT) AS average_sales
+		FROM (SELECT SUM(s.cost) AS total_sales_per_store FROM sales s GROUP BY s.store_id)
+	)
+ON total_sales_per_store > average_sales;
