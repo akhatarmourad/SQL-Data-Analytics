da41fdd1599e0ee76b37f4c5ec6b0c75a591ab26
akhatarmourad <mourad.akhatar01@gmail.com>
2024-10-26 20:35:36 +0100

    Create & Update views

--- Views/2_SQL_Views.sql
+++ Views/2_SQL_Views.sql
@@ -8,7 +8,7 @@ select * from tb_product_info;
 select * from tb_order_details;
 
 -- Select orders details
-CREATE VIEW order_details AS
+CREATE OR REPLACE VIEW order_details AS
 SELECT o.ord_id, o.date, p.prod_name, c.cust_name,
 	(p.price * o.quantity) - ((p.price - o.quantity) * o.disc_percent::float / 100) as total_cost
 	FROM tb_order_details o
@@ -17,4 +17,14 @@ SELECT o.ord_id, o.date, p.prod_name, c.cust_name,
 
 SELECT * FROM order_details;
 
+-- Rename Columns names & View name
+ALTER VIEW order_details RENAME COLUMN ord_id TO order_id;
+ALTER VIEW order_details RENAME COLUMN prod_name TO product_name;
+ALTER VIEW order_details RENAME COLUMN cust_name TO customer_name;
 
+ALTER VIEW order_details RENAME TO order_details_view;
+
+SELECT * FROM order_details_view;
+
+-- Delete a view
+DROP VIEW order_details_view;
