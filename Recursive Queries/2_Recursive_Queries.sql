0ccdcd9a63c3a01bedee45bb9e579560f3b6f104
akhatarmourad <mourad.akhatar01@gmail.com>
2024-10-29 12:05:25 +0100

    Recursive Queries examples

--- Recursive Queries/2_Recursive_Queries.sql
+++ Recursive Queries/2_Recursive_Queries.sql
@@ -1,2 +1,32 @@
 -- SELECT all employees details
 SELECT * FROM emp_details;
+
+-- Global Syntax in PostgreSQL
+WITH [RECURSIVE] CTE_name AS 
+	(
+		Base Query or Initial Query
+		UNION [ALL]
+		Recursive Query that references the CTE + termination condition
+	)
+
+SELECT * FROM CTE_name;
+
+-- Question 1 : loop through numbers from 0 to 10 using recursion
+WITH RECURSIVE numbers_loop AS 
+	(
+		SELECT 1 AS n
+		UNION ALL
+		SELECT n + 1 FROM numbers_loop WHERE n < 10
+	)
+
+SELECT * FROM numbers_loop;
+
+-- Question 2 : Find employees supervised by a given manager
+WITH RECURSIVE manager_employees AS
+	(
+		SELECT id, name, manager_id, 1 AS level FROM emp_details WHERE name = 'Asha'
+		UNION ALL
+		SELECT ED.id, ED.name, ED.manager_id, (level + 1) AS level FROM manager_employees ME JOIN emp_details ED ON ME.id = ED.manager_id
+	)
+
+SELECT * FROM manager_employees;
